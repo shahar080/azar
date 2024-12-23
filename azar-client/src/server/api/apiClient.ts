d73c312343c6d7d4e531e6f9507b09e1a2768dae
Shahar Azar <Shahar0080@gmail.com>
2024-12-23 19:26:36 +0200

    Fix add user not working

--- azar-client/src/server/api/apiClient.ts
+++ azar-client/src/server/api/apiClient.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
-// import {logout} from "../../store/authSlice.ts";
+import {logout} from "../../store/authSlice.ts";
 
 // Create an Axios instance
 const apiClient = axios.create({
     baseURL: "https://shahar-azar.com/api",
-//     baseURL: "http://127.0.0.1:8080/api",
+    // baseURL: "http://127.0.0.1:8080/api",
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json',
@@ -42,41 +42,41 @@ export async function refreshToken(): Promise<string | null> {
 }
 
 
-// apiClient.interceptors.request.use((config) => {
-//     const token = localStorage.getItem('authToken');
-//     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-// }, (error) => Promise.reject(error));
-//
-// apiClient.interceptors.response.use(
-//     (response) => response, // Pass valid responses through
-//     async (error) => {
-//         console.error('Interceptor caught error:', error);
-//
-//         const originalRequest = error.config;
-//
-//         if (error.response && error.response.status === 401 && !originalRequest._retry) {
-//             originalRequest._retry = true;
-//
-//             try {
-//                 const newToken = await refreshToken();
-//                 if (newToken) {
-//
-//                     // Update request headers with new token
-//                     originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
-//
-//                     // Retry the failed request
-//                     return apiClient(originalRequest);
-//                 }
-//             } catch (refreshError) {
-//                 console.error('Failed to refresh token:', refreshError);
-//                 logout(); // Redirect to login if refresh fails
-//             }
-//         }
-//
-//         return Promise.reject(error); // Reject if token refresh fails
-//     }
-// );
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => Promise.reject(error));
+
+apiClient.interceptors.response.use(
+    (response) => response, // Pass valid responses through
+    async (error) => {
+        console.error('Interceptor caught error:', error);
+
+        const originalRequest = error.config;
+
+        if (error.response && error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const newToken = await refreshToken();
+                if (newToken) {
+
+                    // Update request headers with new token
+                    originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+
+                    // Retry the failed request
+                    return apiClient(originalRequest);
+                }
+            } catch (refreshError) {
+                console.error('Failed to refresh token:', refreshError);
+                logout(); // Redirect to login if refresh fails
+            }
+        }
+
+        return Promise.reject(error); // Reject if token refresh fails
+    }
+);
 export default apiClient;

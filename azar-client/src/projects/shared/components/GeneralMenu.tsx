2ad6584e50ecef983f0c47eef28435a229ab5e76
Shahar Azar <shahar0080@gmail.com>
2025-01-30 22:41:27 +0000

    Merged in AZAR-106-client-make-the-menu-dropdown-t (pull request #81)
    AZAR-106 make menu dropdown nicer
    
    * AZAR-106 make menu dropdown nicer

--- azar-client/src/projects/shared/components/GeneralMenu.tsx
+++ azar-client/src/projects/shared/components/GeneralMenu.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {Box, Button, Menu, MenuItem} from "@mui/material";
+import {ArrowDropDown} from "@mui/icons-material";
 import {Link} from "react-router-dom";
 import {CLOUD_ROUTE, LANDING_ROUTE, WEATHER_ROUTE} from "../utils/reactRoutes.ts";
 
@@ -32,29 +33,89 @@ export function GeneralMenu({zIndex = 1}: GeneralMenuProps) {
                     onClick={handleClick}
                     variant="contained"
                     sx={{
-                        backgroundColor: "primary",
-                        color: "secondary",
+                        backgroundColor: "primary.main",
+                        color: "secondary.contrastText",
                         textTransform: "none",
-                        borderRadius: "10vw"
+                        borderRadius: "8px",
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "8px",
                     }}
                 >
                     Menu
+                    <ArrowDropDown/> {/* Down arrow icon */}
                 </Button>
                 <Menu
                     anchorEl={anchorEl}
                     open={Boolean(anchorEl)}
                     onClose={handleClose}
+                    sx={{
+                        mt: "0px",
+                        "& .MuiPaper-root": {
+                            backgroundColor: "transparent",
+                            boxShadow: "none",
+                        },
+                        "& .MuiMenu-list": {
+                            padding: "0",
+                            backgroundColor: "primary.main",
+                            borderRadius: "0 0 8px 8px",
+                            boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.25)",
+                        },
+                    }}
                 >
-                    <MenuItem onClick={handleClose} component={Link} to={LANDING_ROUTE}>
+                    <MenuItem
+                        onClick={handleClose}
+                        component={Link}
+                        to={LANDING_ROUTE}
+                        sx={{
+                            padding: "12px 20px",
+                            margin: "0",
+                            borderRadius: "0 8px 0 0",
+                            color: "white",
+                            "&:hover": {
+                                backgroundColor: "primary.dark",
+                                color: "secondary.contrastText",
+                            },
+                        }}
+                    >
                         Shahar
                     </MenuItem>
-                    <MenuItem onClick={handleClose} component={Link} to={CLOUD_ROUTE}>
+                    <MenuItem
+                        onClick={handleClose}
+                        component={Link}
+                        to={CLOUD_ROUTE}
+                        sx={{
+                            padding: "12px 20px",
+                            margin: "0",
+                            borderRadius: "0",
+                            color: "white",
+                            "&:hover": {
+                                backgroundColor: "primary.dark",
+                                color: "secondary.contrastText",
+                            },
+                        }}
+                    >
                         AzarCloud
                     </MenuItem>
-                    <MenuItem onClick={handleClose} component={Link} to={WEATHER_ROUTE}>
+                    <MenuItem
+                        onClick={handleClose}
+                        component={Link}
+                        to={WEATHER_ROUTE}
+                        sx={{
+                            padding: "12px 20px",
+                            margin: "0",
+                            borderRadius: "0 0 8px 8px",
+                            color: "white",
+                            "&:hover": {
+                                backgroundColor: "primary.dark",
+                                color: "secondary.contrastText",
+                            },
+                        }}
+                    >
                         Weather
                     </MenuItem>
                 </Menu>
+
             </Box>
         </>
     );
